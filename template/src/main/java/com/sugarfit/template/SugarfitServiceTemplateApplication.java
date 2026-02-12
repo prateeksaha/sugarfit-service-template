d20d3bb144e8adcf0436c5c76bdfab3f3ed8272c
Prateek Saha <sahaprateek28@gmail.com>
2026-02-12 11:20:23 +0530

    Initial spring boot project setup

---
+++ template/src/main/java/com/sugarfit/template/SugarfitServiceTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.sugarfit.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarfitServiceTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarfitServiceTemplateApplication.class, args);
+	}
+
+}
